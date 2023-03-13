c4fc4324e7df3f2d23910d080e7ec627c69d4868
skoo <skoo@kaist.ac.kr>
2023-03-14 00:12:29 +0900

    empty_world

---
+++ empty_world_py/setup.py
@@ -0,0 +1,28 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'empty_world_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='skoo',
+    maintainer_email='skoo@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
